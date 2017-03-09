e7821be777d8d411d40777a27bd9e82d75209a0a
Daniel O'Brien <15dobrien@stjosephsrush.com>
2017-03-09 15:22:15 +0000

    Shareio
    Share, Now!

---
+++ shareio.py
@@ -0,0 +1,20 @@
+#BETA
+#Share.io Version 1.0 on Github: @Share.io
+#Share.io is an Open-Source Application for Personal/Home Use
+#More Info: share.io
+print("Welcome to Share.io")
+first=(int(input("Please enter the Amount of Money you are sharing/dividing")))
+print("In this program we will use the following variables in our Ratio; x:y")
+second=(int(input("Please enter the number for 'x'")))
+third=(int(input("Please enter the number for 'y'")))
+fourth = second+third
+fifth = first/fourth
+sixth = second*fifth
+seventh = third*fifth
+sixth = (str(sixth))
+seventh = (str(seventh))
+print("Your Ratio is.........")
+print(sixth + ":" + seventh)
+print("Thank You for using Share.io!!")
+#Copyright Share.io 2017
+#Developed by @daniel-obrien
